d33db8137ac68da2f5de586bacf5edad51086ca4
ngakana <nlsalemane@gmail.com>
2021-08-15 18:24:38 +0200

    ✨ add user prop

--- src/components/ChatMessageList.jsx
+++ src/components/ChatMessageList.jsx
@@ -5,7 +5,6 @@ function ChatMessageList() {
 
     return(
         <ul className="message-list">
-<<<<<<< HEAD
             <ChatMessage user={"@sal"} byUser={true} />
             <ChatMessage user={"@therock"} byUser={false} />
             <ChatMessage user={"@therock"} byUser={false} />
@@ -22,24 +21,6 @@ function ChatMessageList() {
             <ChatMessage user={"@sal"} byUser={true} />
             <ChatMessage user={"@therock"} byUser={false} />
             <ChatMessage user={"@therock"} byUser={false} />
-=======
-            <ChatMessage byUser={true} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={true} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={true} />
-            <ChatMessage byUser={false} />
-            <ChatMessage byUser={false} />
->>>>>>> b337fb598fc9b2a1d2015f0ab020411ee7044594
         </ul>
     );
 }
