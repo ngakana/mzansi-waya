4241b5f09e087a483a9efc4f8cef26d73dc2f7c9
ngakana <nlsalemane@gmail.com>
2021-08-17 00:12:32 +0200

    🚧 shuffle and repeat buttons component skeleton

---
+++ src/components/PlaybackSettingsControls.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+
+import { ReactComponent as ShuffleButton } from "assets/icons/shuffle.svg";
+import { ReactComponent as RepeatButton } from "assets/icons/repeat-alt.svg";
+import { ReactComponent as RepeatOneButton } from "assets/icons/repeat-1-alt.svg";
+
+import { PlaybackSettingsContext } from "hooks/PlaybackSettingsContext";
+
+function PlaybackSettingsControls() {
+
+    const { playbackSettings, toggleShuffle, toggleRepeat }= useContext(PlaybackSettingsContext);
+
+    const shuffleHandler = () => {
+        toggleShuffle();
+    }
+
+    const repeatHandler = () => {
+        toggleRepeat();
+    }
+
+    return(
+        <div className="playback-settings">
+            {
+                playbackSettings.shuffle ?
+                <ShuffleButton className="shuffle-on" onClick={shuffleHandler} /> 
+                :
+                <ShuffleButton className="shuffle-off" onClick={shuffleHandler} />
+            }
+            {
+                playbackRepeat === "off" ?
+                <RepeatButton className="repeat-off" onClick={repeatHandler} />
+                :
+                playbackRepeat === "on" ?
+                <RepeatButton className="repeat-on" onClick={repeatHandler} />
+                :
+                <RepeatOneButton className="repeat-one" onClick={repeatHandler} />
+            }
+        </div>
+    );
+}
+export default PlaybackSettingsControls;
