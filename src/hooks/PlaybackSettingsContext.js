18618d0d2e4fce40a07c83800364f0a8f0eec6fd
ngakana <nlsalemane@gmail.com>
2021-08-17 00:10:48 +0200

    ✨ shuffle and repeat settings context first commit

---
+++ src/hooks/PlaybackSettingsContext.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+
+export const PlaybackSettingsContext = createContext();
+
+export const PlaybackSettingsProvider = ({props}) => {
+    
+    const [playbackSettings, setPlaybackSettings] = useState({
+        shuffle: false,
+        repeat: "off"
+    });
+
+    const toggleShuffle = () => {
+        setPlaybackSettings( (state) => {
+            return {...state, shuffle: !state.shuffle } 
+        });
+    }
+
+    const toggleRepeat = () => {
+        setPlaybackSettings( (state) => {
+            if (state.repeat === "off") {
+                return {...state, repeat: "on"}
+            } else if ( state.repeat === "on" ) {
+                return {...state, repeat: "repeat-one"}
+            } else if ( state.repeat === "repeat-one" ) {
+                return {...state, repeat: "off"}
+            }
+        });
+    }
+
+    return(
+        <PlaybackSettingsProvider value={{ playbackSettings, toggleShuffle, toggleRepeat }} >
+            {props.children}
+        </PlaybackSettingsProvider>
+    );
+}
