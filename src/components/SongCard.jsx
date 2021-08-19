fe5febbced721d6b9c92664152575cd54e1a31f8
ngakana <nlsalemane@gmail.com>
2021-08-19 20:45:38 +0200

    🚧 song card component

---
+++ src/components/SongCard.jsx
@@ -0,0 +1,21 @@
+import InfiniteTextScroll from "./InfiniteTextScroll";
+
+function SongCard({artwork, title, artist, featured}) {
+    let name = [];
+    name[0] = title;
+
+    let artists = [];
+    artists.unshift(artist, featured);
+
+    return(
+        <div className="song-card">
+            <img src={artwork} alt="" />
+            <div className="song-details">
+                <InfiniteTextScroll items={name} />
+                <InfiniteTextScroll items={ artists } />
+            </div>
+        </div>
+    );
+}
+
+export default SongCard;
