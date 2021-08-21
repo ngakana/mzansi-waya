63294e77d8c7831ffb2f4faf0fe713d77c67880e
ngakana <nlsalemane@gmail.com>
2021-08-21 21:09:38 +0200

    🚧 add audio component

--- src/components/MediaPlayControls.jsx
+++ src/components/MediaPlayControls.jsx
@@ -1,10 +1,19 @@
+import { useRef, useContext } from "react";
 
 import { ReactComponent as SkipBackButton } from "assets/icons/backward-step.svg";
 import { ReactComponent as SkipForwardButton } from "assets/icons/forward-step.svg";
 import { ReactComponent as PlayButton } from "assets/icons/play.svg";
 import { ReactComponent as PauseButton } from "assets/icons/pause.svg";
 
+import { PlayingSongContext } from "hooks/PlayingSongContext";
+import { MediaPlayControlsContext } from "hooks/MediaPlayControlsContext";
+
 function MediaPlayControls({songIsPlaying}) {
+
+    const audioRef = useRef(null);
+
+    const { playingSong, setPlayingSong, changePlayingSong } = useContext(PlayingSongContext);
+    const { playButtonHandler, skipButtonHandler, seekHandler, songEndHandler } = useContext(MediaPlayControlsContext);
 
     return(
         <div className="media-play-controls">
@@ -16,6 +25,13 @@ function MediaPlayControls({songIsPlaying}) {
                 <PlayButton style={{ transform: "scale(2)" }} />
             }
             <SkipForwardButton />
+            <audio
+                // onTimeUpdate={songTimeUpdatehandler} 
+                // onLoadedMetadata={songTimeUpdatehandler}
+                ref={audioRef}
+                src={playingSong.song.audio}
+                onEnded={songEndHandler}
+            ></audio>
         </div>
     );
 }
