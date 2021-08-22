f80fb3360638ccfb99b8935fbe461ca35a47c00a
ngakana <nlsalemane@gmail.com>
2021-08-22 19:50:21 +0200

    🐛 bug: change all state props on song change

--- src/hooks/PlayingSongContext.js
+++ src/hooks/PlayingSongContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 
 import { SongsContext } from "./SongsContext";
 
@@ -9,11 +9,7 @@ export const PlayingSongProvider = (props) => {
     const { songs, setSongs } = useContext(SongsContext);
     
     const [playingSong, setPlayingSong] = useState( () => {
-        const prevState = localStorage.getItem("lastplayed");
-        return prevState !== null ? 
-            prevState 
-            :
-            {
+        return {
                 "song": songs[0],
                 "len": 0,
                 "playedLen": 0,
@@ -22,18 +18,17 @@ export const PlayingSongProvider = (props) => {
             }
     });
 
-    useEffect( () => {
-        localStorage.removeItem("lastplayed");
-        localStorage.setItem("lastplayed", JSON.stringify(playingSong));
-    }, [playingSong]);
-
     const changePlayingSong = (desiredSong) => {
 
         const updatedSongs = songs.map( song => {
             if ( song.id === desiredSong.id ) {
                 const track = {...song, active: true}
-                setPlayingSong( state => {
-                    return {...state, song:track}
+                setPlayingSong({
+                    "song": song,
+                    "len": 0,
+                    "playedLen": 0,
+                    "playedLenPercentage": 0,
+                    "playing": false,
                 });
                 
                 return song;
