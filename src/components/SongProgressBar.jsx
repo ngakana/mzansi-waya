48d80b1934aac7278fc39e80f073e9acd32bf1a5
ngakana <nlsalemane@gmail.com>
2021-08-22 19:45:55 +0200

    🚧 format time display

--- src/components/SongProgressBar.jsx
+++ src/components/SongProgressBar.jsx
@@ -1,20 +1,23 @@
 import { useContext } from "react";
 
 import { PlayingSongContext } from "hooks/PlayingSongContext";
+import { MediaPlayControlsContext } from "hooks/MediaPlayControlsContext";
 
 import RangeInput from "./RangeInput";
 
 function SongProgressBar() {
 
-    const { playingSong, setPlayingSong, changePlayingSong } = useContext(PlayingSongContext);
+    const { playingSong } = useContext(PlayingSongContext);
+    const { formatTimeInfo, seekHandler } = useContext(MediaPlayControlsContext);
+
+    let startTime = formatTimeInfo(playingSong.playedLen);
+    let endTime = formatTimeInfo(playingSong.len);
 
     return(
         <div className="song-progress-bar">
-            {/* <p>{playingSong.playedLen}</p> */}
-            <p>0:00:00</p>
-            <RangeInput name="track" max="100" />
-            {/* <p>{playingSong.len}</p> */}
-            <p>3:10:00</p>
+            <p>{startTime}</p>
+            <RangeInput name="track" max={playingSong.len} progress={playingSong.playedLen} changeHandler={seekHandler}  />
+            <p>{endTime}</p>
         </div>
     );
 }
