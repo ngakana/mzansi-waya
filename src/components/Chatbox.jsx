fcde6a4a440639e5fc8c082c052268601013fc53
ngakana <nlsalemane@gmail.com>
2021-08-25 19:52:31 +0200

    ✨ add toggle button

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 
 import Typebox from "./Typebox";
 import ChatMessageList from "./ChatMessageList";
+import { ReactComponent as Close } from "assets/icons/angle-double-up.svg";
+import { ReactComponent as Open } from "assets/icons/angle-double-down.svg";
 
 function Chatbox() {
 
+    const [chatIsOpen, setChatIsOpen] = useState(true);
+
+    const toggleChatbox = () => {
+        setChatIsOpen(!chatIsOpen);
+    }
+
     return(
-        <div className="chatbox">
+        <div className={`chatbox ${ chatIsOpen ? null : "closed-chatbox" }`}>
             <div className="chat-header">
-                <h1 className="text">Chat</h1>
+                <h2 className="text">Chat</h2>
+                { chatIsOpen ? <Close onClick={toggleChatbox} /> : <Open onClick={toggleChatbox} /> }
             </div>
             <ChatMessageList />
             <Typebox />
