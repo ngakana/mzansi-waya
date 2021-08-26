0cb569eb1b16b128944b1f279e2db9e09ab9c6a2
ngakana <nlsalemane@gmail.com>
2021-08-26 21:42:38 +0200

    🚸 add play button on library song cards

--- src/components/SongsLibrary.jsx
+++ src/components/SongsLibrary.jsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import { v4 as uuidv4 } from "uuid";
 
+import { ReactComponent as PlayIcon } from "assets/icons/play-circle.svg";
+import { ReactComponent as PauseIcon } from "assets/icons/pause-circle.svg";
+
 import SongCard from "./SongCard";
 
 import { SongsContext } from "hooks/SongsContext";
@@ -10,14 +13,15 @@ import { MediaPlayControlsContext } from "hooks/MediaPlayControlsContext";
 function SongsLibrary() {
 
     const { songs } = useContext(SongsContext);
-    const { audioRef } = useContext(MediaPlayControlsContext);
-    const { changePlayingSong, setPlayingSong } = useContext(PlayingSongContext);
+    const { audioRef, playButtonHandler } = useContext(MediaPlayControlsContext);
+    const { playingSong, changePlayingSong, setPlayingSong } = useContext(PlayingSongContext);
 
     const handleChangeSong = async (event) => {
-        let wasPlaying = true;
-        if ( audioRef.current.paused ) {
-            wasPlaying = false;
+        let wasPlaying = false;
+        if ( playingSong.playing  ) {
+            wasPlaying = true;
         }
+
         await changePlayingSong(event.currentTarget.id);
 
         if (wasPlaying) {
@@ -27,7 +31,35 @@ function SongsLibrary() {
             setPlayingSong( state => {
                 return {...state, playing: true}
             });
+        } else {
+            setTimeout(() => {
+                audioRef.current.pause();
+            }, 0.1);
+            setPlayingSong( state => {
+                return {...state, playing: false}
+            });
         }
+    }
+
+    const playSongOnFocus = (event) => {
+        let wasPlaying = false;
+        if ( playingSong.playing  ) {
+            wasPlaying = true;
+        }
+
+        if (wasPlaying) {
+            audioRef.current.pause();
+            setPlayingSong( state => {
+                return {...state, playing: false}
+            });
+        } else {
+            audioRef.current.play();
+            setPlayingSong( state => {
+                return {...state, playing: true}
+            });
+        }
+
+        console.log(event);
     }
 
     return(
@@ -40,6 +72,12 @@ function SongsLibrary() {
                         className={ song.active ? "selected-song" : "not active" }
                         onClick={handleChangeSong} 
                     >
+                        {
+                            song.id === playingSong.song.id && playingSong.playing ?
+                            <PauseIcon onClick={playButtonHandler} />
+                            :
+                            <PlayIcon onClick={playButtonHandler} />
+                        }
                         <SongCard 
                             artwork={song.artwork}
                             title={song.title}
